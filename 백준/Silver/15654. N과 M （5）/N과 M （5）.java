0d85d5862deb00c5d164448523b7ce91a1a4a0e4
khjjj_ <108385400+wjdrbgus8167@users.noreply.github.com>
2024-09-04 23:51:14 +0900

    [Silver III] Title: N과 M (5), Time: 1836 ms, Memory: 67352 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,56 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N,M;
+	static boolean visited[];
+	static int arr[];
+	static int arr1[];
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		arr1 = new int[N];
+		visited = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0;i<N;i++) {
+			
+			arr[i] =Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		permutation(0);
+		
+	}
+	public static void permutation(int cnt) {
+		if(cnt==M) {
+			
+			for(int i=0;i<M;i++) {
+					System.out.print(arr1[i]+" ");
+				
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=0;i<N;i++) {
+			
+			if(!visited[i]) {
+				visited[i]=true;
+				arr1[cnt]=arr[i];
+				permutation(cnt+1);
+				visited[i]=false;
+				
+			}
+		}
+	}
+
+}

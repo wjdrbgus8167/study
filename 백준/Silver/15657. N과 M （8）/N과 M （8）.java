c94998923e471cfa2321e3162d3abbd28c0a28a2
khjjj_ <108385400+wjdrbgus8167@users.noreply.github.com>
2024-09-06 01:13:14 +0900

    [Silver III] Title: N과 M (8), Time: 196 ms, Memory: 21312 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,55 @@
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+	static int N,M;
+	static boolean visited[];
+	static int arr[];
+	static int arr1[];
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		arr1 = new int[N];
+		visited = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0;i<N;i++) {
+			
+			arr[i] =Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		permutation(0,0);
+		System.out.println(sb);
+	}
+	public static void permutation(int cnt,int  ecnt) {
+		if(cnt==M) {
+			
+			for(int i=0;i<M;i++) {
+				sb.append(arr1[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=ecnt;i<N;i++) {
+			
+			
+			arr1[cnt]=arr[i];
+			permutation(cnt+1,i);
+				
+			
+		}
+	}
+
+}

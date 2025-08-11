404b95e30aa3fee5c9d57f2b11ef1da574b00c27
Khyeon_99 <108385400+wjdrbgus8167@users.noreply.github.com>
2025-08-11 16:14:08 +0900

    [Silver III] Title: N과 M (8), Time: 184 ms, Memory: 22168 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,55 +1,53 @@
-
-
-
 import java.io.*;
 import java.util.*;
 
 public class Main{
 
-	static int N,M;
-	static boolean visited[];
-	static int arr[];
-	static int arr1[];
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args)throws IOException {
-		// TODO Auto-generated method stub
+    static int N,M;
+    static int arr[];
+    static int result[];
+    static StringBuilder sb = new StringBuilder();
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		arr1 = new int[N];
-		visited = new boolean[N];
-		st = new StringTokenizer(br.readLine());
-		
-		for(int i=0;i<N;i++) {
-			
-			arr[i] =Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		permutation(0,0);
-		System.out.println(sb);
-	}
-	public static void permutation(int cnt,int  ecnt) {
-		if(cnt==M) {
-			
-			for(int i=0;i<M;i++) {
-				sb.append(arr1[i]+" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i=ecnt;i<N;i++) {
-			
-			
-			arr1[cnt]=arr[i];
-			permutation(cnt+1,i);
-				
-			
-		}
-	}
+    public static void main(String[] args)throws IOException{
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        result = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i =0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0,0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int cnt, int depth){
+
+        if(depth == M){
+            for(int a :result){
+                sb.append(a+" ");
+            }
+            sb.append("\n");
+
+            return ;
+        }
+        
+        for(int i=cnt;i<N;i++){
+            result[depth] = arr[i];
+            dfs(i,depth+1);
+
+        }
+
+    }
 }

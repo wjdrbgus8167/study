4b5a26490c51b8eee80c214f10f7dbe1840d27db
Khyeon_99 <108385400+wjdrbgus8167@users.noreply.github.com>
2025-08-12 23:43:17 +0900

    [Gold V] Title: 토마토, Time: 524 ms, Memory: 103820 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,122 +1,102 @@
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 import java.io.BufferedReader;
-import java.util.*;
 
 class Point{
     int x,y;
 
     public Point(int x,int y){
         this.x = x;
-        this.y =y;
+        this.y = y;
+
     }
 
 }
+class Main{
 
-public class Main{
-
-    static int min_cnt=0;
     static int N,M;
     static int tomato[][];
-    static int dx[] = {1,0,-1,0,};
-    static int dy[] = {0,1,0,-1};
     static boolean visited[][];
-    static int tomato_cnt=0;
-    static ArrayList<Point> list;
+    static int tomato_cnt;
+    static int min_day=0;
+    static int dx[] = {1,0,-1,0};
+    static int dy[] = {0,1,0,-1};
+    static ArrayDeque<Point> que;
 
     public static void main(String[] args)throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        que = new ArrayDeque<>();
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         tomato = new int[M][N];
-        list = new ArrayList<>();
         visited = new boolean[M][N];
-
 
         for(int i=0;i<M;i++){
 
-            st = new StringTokenizer(br.readLine());
-            
-            for(int j=0;j<N;j++){
-                
-                tomato[i][j] = Integer.parseInt(st.nextToken());
-                
-                if(tomato[i][j]==0){
-                    tomato_cnt++;
-                }else if(tomato[i][j]==1){
+            st= new StringTokenizer(br.readLine());
 
-                    list.add(new Point(i,j));
-                    visited[i][j] = true;
+            for(int j=0;j<N;j++){
+
+                tomato[i][j] = Integer.parseInt(st.nextToken());
+
+                if(tomato[i][j]==1){
+                    que.add(new Point(i,j));
+                }else if(tomato[i][j]==0){
+
+                    tomato_cnt++;
                 }
             }
+
         }
 
-        tomato_ripe();
+        ripe();
 
-        if(tomato_cnt==0){
-            System.out.println(min_cnt);
-        }else{
+        if(tomato_cnt!=0){
             System.out.println(-1);
+        }else{
+            System.out.println(min_day);
         }
 
     }
+    public static void ripe(){
 
-    public static void tomato_ripe(){
+        while(!que.isEmpty()){
 
-       Queue<Point> que = new LinkedList<>();
+            int size= que.size();
 
-       for(int i=0;i<list.size();i++){
-           que.add(list.get(i));
-       }
+            if(tomato_cnt==0){
+                return ;
+            }
+
+            min_day++;
+
+            for(int j=0;j<size;j++){
+                Point p = que.poll();
+                for(int i=0;i<4;i++){
+
+                    int nx = dx[i]+p.x;
+                    int ny = dy[i]+p.y;
+
+                    if(nx<0||nx>=M||ny<0||ny>=N) continue;
+
+                    if(tomato[nx][ny]==0){
+                        tomato[nx][ny]=1;
+                        que.add(new Point(nx,ny));
+                        tomato_cnt--;
+                    }
 
 
-       while(!que.isEmpty()){
+            }
 
-           int size = que.size();
+            }
 
-           if(tomato_cnt==0){
-               return ;
-           }
-
-           min_cnt++;
-
-           for(int i=0;i<size;i++){
-              
-               Point p = que.poll();
-              int x = p.x;
-              int y = p.y;
-
-               for(int j=0;j<4;j++){
-
-                   int next_x = dx[j]+x;
-                   int next_y = dy[j]+y;
-
-                   if(bound_checking(next_x,next_y)&&!visited[next_x][next_y]){
-
-                       if(tomato[next_x][next_y]==0){
-                           
-                           tomato_cnt--;
-                           tomato[next_x][next_y]=1;
-                           visited[next_x][next_y] =true;
-
-                           Point point = new Point(next_x,next_y);
-                           que.add(point);
-                       }
-
-                   }
-               }
-
-           }
-
-       }
-    }
-
-    public static boolean bound_checking(int x,int y){
-
-        return x>=0&&y>=0&&x<M&&y<N;
+        }
     }
 }

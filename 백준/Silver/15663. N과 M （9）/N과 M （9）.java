9dce9e8f4c187eb7289bdd1ea9422fc01a891c99
Khyeon_99 <108385400+wjdrbgus8167@users.noreply.github.com>
2025-08-12 16:30:31 +0900

    [Silver II] Title: N과 M (9), Time: 236 ms, Memory: 28864 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+
+    static int N,M;
+    static int arr[];
+    static boolean visited[];
+    static int result[];
+    static StringBuilder sb = new StringBuilder();
+
+   public static void main(String[] args) throws IOException{
+
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        visited = new boolean[N];
+        result = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        permutation(0);
+
+        System.out.println(sb);
+
+   }
+
+   public static void permutation(int depth){
+
+       if(depth == M ){
+
+           for(int v : result){
+               sb.append(v+" ");
+           }
+
+            sb.append("\n");
+
+           return ;
+       }
+        int prev = Integer.MAX_VALUE;
+
+       for(int i=0;i<N;i++){
+
+           if(!visited[i]&& arr[i] != prev){
+               result[depth] = arr[i];
+               visited[i] = true;
+               prev  = arr[i];
+
+               permutation(depth+1);
+               visited[i] = false;
+           }
+
+       }
+   }
+}

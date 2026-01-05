457d0eb8ea57281e24c686559af1a92abf2478a2
Khyeon_99 <108385400+wjdrbgus8167@users.noreply.github.com>
2026-01-05 19:39:56 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+
+class Solution {
+    public int solution(String[][] clothes) {
+        
+        HashMap<String,Integer> map = new HashMap<>();
+        
+        for(int i=0;i<clothes.length;i++){
+            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
+        }
+        
+        int answer = 1;
+        
+        for(int count : map.values()){
+            answer *=(count+1);
+        }
+        
+        return answer-1;
+    }
+    
+    
+}
